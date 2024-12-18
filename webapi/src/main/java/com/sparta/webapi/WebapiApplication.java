4ceba8414d73e2d1216d08e2b05b321a27224c93
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-18 23:51:38 +0900

    Feat: 서브 모듈의 클래스를 스캔하기 위해 일부 변경

--- webapi/src/main/java/com/sparta/webapi/WebapiApplication.java
+++ webapi/src/main/java/com/sparta/webapi/WebapiApplication.java
@@ -3,7 +3,7 @@ package com.sparta.webapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.sparta", "com.sparta.application.service", "com.sparta.domain.service"})
 public class WebapiApplication {
 
     public static void main(String[] args) {
