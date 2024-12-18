d1a64b9b7fd115b5d96f170222ea3d7be9eb3cbb
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-18 23:50:57 +0900

    Feat: 입력받은 유저 정보를 바탕으로 각 필드에 대한 유효성 검사를 실시하고, 통과한다면 해당 유저의 이메일주소로 인증 메일을 전송, 검증

--- webapi/src/main/java/com/sparta/webapi/controller/UserController.java
+++ webapi/src/main/java/com/sparta/webapi/controller/UserController.java
@@ -1,25 +1,45 @@
 package com.sparta.webapi.controller;
 
+import com.sparta.application.service.EmailService;
 import com.sparta.domain.dto.UserSignupRequestDto;
+import com.sparta.domain.entity.User;
+import com.sparta.domain.service.TokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api")
 public class UserController {
+
+    private final EmailService emailService;
+    private final TokenService tokenService;
+    private String TOKEN = "";
+
+    public UserController(EmailService emailService, TokenService tokenService) {
+        this.emailService = emailService;
+        this.tokenService = tokenService;
+    }
 
     @Operation(summary = "회원가입 - 이메일 인증", description = "사용자가 이메일을 통해 회원가입을 진행합니다.")
     @ApiResponse(responseCode = "202", description = "인증메일 전송 성공")
-    @PostMapping("/signup")
+    @PostMapping("/user/signup")
     public ResponseEntity<Map<String, String>> signup(@Valid @RequestBody UserSignupRequestDto userRequest) {
-
+        log.info("회원가입 - 이메일 인증 컨트롤러 진입");
+        String email = userRequest.getUserEmail();
+        TOKEN = tokenService.generateToken();
+        String subject = "회원가입 인증 이메일!";
+        String body = "http://localhost:8084/api/auth/verify?token=" + TOKEN;
+        emailService.sendEmail(email, subject, body);
         return ResponseEntity
                 .status(202)
                 .body(Map.of("msg",
@@ -28,10 +48,21 @@ public class UserController {
                                 userRequest.getUserEmail())));
     }
 
-    @Operation(summary = "이메일 인증 확인", description = "이메일 인증을 확인하는 엔드포인트입니다.")
-    @GetMapping("/verify-email")
-    public Map<String, String> verifyEmail(@RequestParam String token) {
-        return Map.of("msg", "이메일 인증이 완료되었습니다!");
+    @Operation(summary = "이메일 인증 확인", description = "이메일 인증을 확인하는 엔드포인트")
+    @GetMapping("/auth/verify")
+    public ResponseEntity<String> verifyEmail(@RequestParam String token) {
+        // 토큰 검증 로직
+        if (isValidToken(token)) {
+            return ResponseEntity.ok("이메일 인증이 완료되었습니다!");
+        }
+        return ResponseEntity.status(403).body("유효하지 않은 토큰입니다.");
+    }
+
+    private boolean isValidToken(String token) {
+        if (TOKEN.equals(token)) {
+            return true;
+        }
+        return false;
     }
 
     @Operation(summary = "로그인", description = "JWT 토큰을 이용한 로그인 기능")
