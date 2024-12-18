50a56509ab33e252ab4c8ceb0fa478a6b3c52277
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-18 23:51:12 +0900

    Feat: 토큰을 생성하기 위한 클래스

---
+++ domain/src/main/java/com/sparta/domain/service/TokenService.java
@@ -0,0 +1,13 @@
+package com.sparta.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class TokenService {
+
+    public String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+}
