ff37d70aca5ca10940a97da26c62d726e300969b
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-19 13:08:28 +0900

    Modify: 다시 각 모듈별 필요한 모듈 의존성 추가 및 메인클래스 스캔경로 변경

--- webapi/src/main/java/com/sparta/webapi/WebapiApplication.java
+++ webapi/src/main/java/com/sparta/webapi/WebapiApplication.java
@@ -2,10 +2,13 @@ package com.sparta.webapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.sparta", "com.sparta.application.service", "com.sparta.domain.service"})
+@EntityScan(basePackages = "com.sparta.domain.entity")
+@EnableJpaRepositories(basePackages = "com.sparta.domain.repository")
+@SpringBootApplication(scanBasePackages = "com.sparta")
 public class WebapiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebapiApplication.class, args);
     }
