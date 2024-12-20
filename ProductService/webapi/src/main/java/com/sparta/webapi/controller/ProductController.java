0eb3cb36b16cbf416c48f27e7d61faa0b17ea0c0
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-21 07:30:44 +0900

    Modify: gradle modifying

--- ProductService/webapi/src/main/java/com/sparta/webapi/controller/ProductController.java
+++ ProductService/webapi/src/main/java/com/sparta/webapi/controller/ProductController.java
@@ -1,23 +1,20 @@
 package com.sparta.webapi.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/products")
+@RequestMapping("/api/product")
 public class ProductController {
 
     @Operation(summary = "상품 리스트 조회", description = "등록된 상품 목록을 조회합니다.")
     @GetMapping
-    public List<Map<String, Object>> getProducts() {
-        return List.of(
-                Map.of("productId", 1, "productName", "상품1", "productPrice", 10000, "stockQuantity", 20),
-                Map.of("productId", 2, "productName", "상품2", "productPrice", 20000, "stockQuantity", 15)
-        );
+    public ResponseEntity<Map<String, String>> getProducts() {
+        return null;
     }
 
     @Operation(summary = "상품 상세 정보 조회", description = "특정 상품의 상세 정보를 조회합니다.")
