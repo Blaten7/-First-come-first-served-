30438b15fbf674b91f27aef788099b94887979e1
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-21 00:54:30 +0900

    Modify: etc setup file modifying

---
+++ ProductService/application/src/main/java/com/sparta/application/ProductService_Application_Config.java
@@ -0,0 +1,10 @@
+package com.sparta.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductService_Application_Config {
+
+}
---
+++ ProductService/domain/src/main/java/com/sparta/domain/ProductService_Domain_Config.java
@@ -0,0 +1,10 @@
+package com.sparta.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProductService_Domain_Config {
+
+}
