ceaa323b71461d08bcc3d1b7b5d3b001c5ae041d
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-21 00:54:49 +0900

    Modify: etc setup file modifying

--- UserService/webapi/src/main/java/com/sparta/webapi/UserServiceApplication.java
+++ UserService/webapi/src/main/java/com/sparta/webapi/UserServiceApplication.java
@@ -3,14 +3,15 @@ package com.sparta.webapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @EntityScan(basePackages = "com.sparta.domain.entity")
 @EnableJpaRepositories(basePackages = "com.sparta.domain.repository")
 @SpringBootApplication(scanBasePackages = "com.sparta")
-public class WebapiApplication {
+public class UserServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WebapiApplication.class, args);
+        SpringApplication.run(UserServiceApplication.class, args);
     }
-
 }
