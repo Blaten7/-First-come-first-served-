11ab239f58ad7b234ea447a3ed73e157620b2ef1
Blaten7 <anrqlcdmlrja@naver.com>
2024-12-29 22:20:38 +0900

    Feat: Retry, TimeLimiter add.

--- OrderService/src/main/java/com/sparta/orderservice/connector/ProductServiceConnector.java
+++ OrderService/src/main/java/com/sparta/orderservice/connector/ProductServiceConnector.java
@@ -3,6 +3,7 @@ package com.sparta.orderservice.connector;
 import com.sparta.orderservice.handler.CustomException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,7 +18,9 @@ public class ProductServiceConnector {
     public ProductServiceConnector(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8060").build();
     }
+
     @CircuitBreaker(name = "productService", fallbackMethod = "fallbackIsProductExist")
+    @TimeLimiter(name = "productService")
     @Retry(name = "productService")
     public boolean isProductExist(String productName) {
         try {
@@ -37,6 +40,7 @@ public class ProductServiceConnector {
 
     // Fallback 메서드
     public void fallbackIsProductExist(String productName, Throwable throwable) {
+        log.error("Fallback executed due to: {}", throwable.getMessage());
         throw new CustomException("Failed to order product: " + productName + ". Reason: " + throwable.getMessage());
     }
 
